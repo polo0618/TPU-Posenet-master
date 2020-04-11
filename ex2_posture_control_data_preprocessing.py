e924059fd8b6938dc55b51f96d1e41f237403f8a
polotailokyin <polo0000618@hotmail.com>
2020-04-11 19:07:13 +0800

    Update ex2_posture_control_data_preprocessing.py

--- ex2_posture_control_data_preprocessing.py
+++ ex2_posture_control_data_preprocessing.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("data",  help="data")
 
     args = parser.parse_args()
-    data_name = args.data.split('.')[0]
+    data_name = args.data.split('.')[0].split('/')[1]
     data = preprocess(open_data(args.data))
 
     data.to_csv('data/{}_data.csv'.format(data_name), index=False)
